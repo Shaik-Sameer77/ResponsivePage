4e6e80cc2a7898d655ee599a305ff3b103dcd8a7
sameer <sameer.shammu.1514@gmail.com>
2023-05-05 00:11:26 +0530

    DogImageGenerator

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
+import React from 'react';
 import './App.css';
-import logo from './logo.svg';
-
+import DogImage from './components/DogImage';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <button>Get started...</button>
-        </a>
-      </header>
+      <h1>Dog Image Generator</h1>
+      <DogImage />
     </div>
   );
 }
