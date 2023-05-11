25102c0da610d88fb540e82d7beaa85e8187b94a
sameer <sameer.shammu.1514@gmail.com>
2023-05-11 09:18:17 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import "./App.css"
 import CardContainer from './components/CardContainer';
 
 const App = () => {
   return (
     <div>
-      {/* <h1>Card Container Example</h1> */}
+      <h1>Card Container</h1>
       <CardContainer />
     </div>
   );
